523adb96b1d119b5335b238723fb93f2c8d0aa6e
OriRoth22 <126409680+OriRoth22@users.noreply.github.com>
2023-03-12 16:05:06 +0200

    Add files via upload

---
+++ SPL3/SPL231-Assignment3-student-template-2/server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -0,0 +1,25 @@
+package bgu.spl.net.impl.stomp;
+
+
+import bgu.spl.net.srv.Server;
+
+public class StompServer {
+
+    public static void main(String[] args) {
+        // TODO: implement this
+        
+        // you can use any server... 
+        // Server.threadPerClient(
+        //         7777, //port
+        //         () -> new StompProtocol(), //protocol factory
+        //         FrameMessageEncoderDecoder::new //message encoder decoder factory
+        // ).serve();
+    
+        Server.reactor(
+                Runtime.getRuntime().availableProcessors(),
+                7777, //port
+                () -> new StompProtocol(), //protocol factory
+                FrameMessageEncoderDecoder::new //message encoder decoder factory
+        ).serve();
+    }
+}
